02319fa274d46e642dfa7695f0db707123262787
Ali Ghorashi <aligho@mit.edu>
2021-03-24 21:51:41 -0400

    make_test_xsf

--- src/cluster_module.jl
+++ src/cluster_module.jl
@@ -2,5 +2,6 @@ module cluster_module
 
 include("make_cluster.jl")
 export make_xsf
+export make_test_xsf
 
 end # module
--- src/make_cluster.jl
+++ src/make_cluster.jl
@@ -1,13 +1,18 @@
 
-function make_xsf()
+function make_xsf(ionpos::AbstractVector, lattice::Array{<:Real, 2})
+
 run(`createXSF`)
+
 end
 
 function make_test_xsf()
-path_to_test_si = joinpath(@__DIR__, "../examples/si.out")
-run(`cat $path_to_test_si`)
-run(`cp $path_to_test_si si-copy.out`)
-run(`createXSF si-copy.out si.xsf `)
-run(`open si.xsf`)
-run(`rm si-copy.out`)
+    path_to_test_si = joinpath(@__DIR__, "../examples/si.out");
+    print("here")
+    if isfile("si-copy.out")
+        rm("si-copy.out")
+    end
+    run(`cp $path_to_test_si si-copy.out`)
+    run(`createXSF si-copy.out si.xsf `)
+    run(`open si.xsf`)
+    rm("si-copy.out")
 end
