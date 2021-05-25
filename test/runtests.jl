5a2a5a43a67539134888d57edef0abbcaf8ecc64
Ali Ghorashi <aligho@mit.edu>
2021-05-25 13:04:59 -0400

    rotating ions tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,12 @@ using Test, PyCall, cluster_module
 @testset "Cluster_Module" begin
 
 end
+
+@testset "Rotating Ions" begin
+    #Check rotations about z
+    rotateinz([1, 0, 0], π) ≈ [-1, 0, 0]
+    rotateion([1, 0, 0], [0, 0, 0], [0, 1, 0], π) ≈ [-1, 0, 0]
+    findperp([0, 0, 0], [1, 0, 0], [0, 1, 0]) ≈ [0, 0, 1]
+    a, b, c = rand(3), rand(3), rand(3)
+    sum(findperp(a, b, c).*(a-b))
+end
