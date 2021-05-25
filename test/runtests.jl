bd0ba86015dd0425a91948f998ca4783c85a6f3d
Ali Ghorashi <aligho@mit.edu>
2021-05-25 13:51:58 -0400

    fixed ionparsing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,18 +9,18 @@ end
 
 @testset "Rotating Ions" begin
     #Check rotations about z
-    rotateinz([1, 0, 0], π) ≈ [-1, 0, 0]
-    rotateion([1, 0, 0], [0, 0, 0], [0, 1, 0], π) ≈ [-1, 0, 0]
-    findperp([0, 0, 0], [1, 0, 0], [0, 1, 0]) ≈ [0, 0, 1]
+    @test rotateinz([1, 0, 0], π) ≈ [-1, 0, 0]
+    @test rotateion([1, 0, 0], [0, 0, 0], [0, 1, 0], π) ≈ [-1, 0, 0]
+    @test findperp([0, 0, 0], [1, 0, 0], [0, 1, 0]) ≈ [0, 0, 1]
     a, b, c = rand(3), rand(3), rand(3)
-    sum(findperp(a, b, c).*(a-b))
+    @test isapprox(sum(findperp(a, b, c).*(a-b)), 0, atol=1e-3)
 end
 
 @testset "Obtaining angles and distances" begin
-    getangle([0, 0, 0], [1, 0, 0], [-1/2, sqrt(3)/2, 0])*180/π ≈ 120    
-    getdist([0, 0, 0], [√3/2, 1/2, 0]) ≈ 1
+    @test getangle([0, 0, 0], [1, 0, 0], [-1/2, sqrt(3)/2, 0])*180/π ≈ 120    
+    @test getdist([0, 0, 0], [√3/2, 1/2, 0]) ≈ 1
 end
 
-@test "Parsing JDFTX input files" begin
-    ionparser("ion C 0 0.3333333 0 1") ≈ [0, 0.3333333, 0]
+@testset "Parsing JDFTX input files" begin
+    @test ionparser("ion C 0 0.3333333 0 1") ≈ [0, 0.3333333, 0]
 end
